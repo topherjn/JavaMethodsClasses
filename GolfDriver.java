2fa6bbd3d5aa1249741fa2a113122ced5f2a66f8
Christopher Nicholson <nicholsonc@gmail.com>
2025-03-30 21:28:36 -0400

    Added some more logic to simplest Golfer

--- GolfDriver.java
+++ GolfDriver.java
@@ -8,11 +8,32 @@ public class GolfDriver {
         // of the Golfer object
         // Reference of the Golfer object assigned to the Golfer data type
         // variable
-        Golfer g1 = new Golfer();
-        g1.name = "Alice";
-        g1.score = 72;
+        Golfer golfer = new Golfer();
+        golfer.name = "Alice";
+        golfer.score = 72;
 
-        System.out.println(g1.name + " scored " + g1.score);
-        // Output: Alice scored 72
+        // printing the values of the fields of the Golfer object
+        System.out.println("Golfer Name: " + golfer.name);
+
+        // overwriting the values of the fields of the Golfer object
+        // with new values
+        golfer.name = "Bob";
+        golfer.score = 68;
+
+        // printing the new values of the fields of the Golfer object
+        System.out.println("Golfer Name: " + golfer.name);
+        System.out.println("Golfer Score: " + golfer.score);
+
+        // creating a new Golfer object
+        Golfer golfer2 = new Golfer();      
+        golfer2.name = "Charlie";
+        golfer2.score = 70;
+
+        // printing the values of the fields of the new Golfer object       
+        System.out.println("Golfer Name: " + golfer2.name);
+        System.out.println("Golfer Score: " + golfer2.score);
+
+
+        
     }
 }
